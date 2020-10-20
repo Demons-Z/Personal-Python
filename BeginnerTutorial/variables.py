f0daef0d44ea0311350c54845c6b848ffea68afa
zejzhang <zejzhang@iu.edu>
2020-10-20 13:43:40 +0800

    1

---
+++ BeginnerTutorial/variables.py
@@ -0,0 +1,13 @@
+character_name = "Jason"
+character_age = "22"
+print("There once was a man named " + character_name +",")
+print("he was "+character_age+" years old.")
+print("He really liked the name " + character_name +", ")
+print("but didn't like being "+character_age+".")
+
+character_name = "Tim"
+character_age = "40"
+print("There once was a man named " + character_name +",")
+print("he was "+character_age+" years old.")
+print("He really liked the name " + character_name +", ")
+print("but didn't like being "+character_age+".")

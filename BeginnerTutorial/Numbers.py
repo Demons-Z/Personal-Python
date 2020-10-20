6575ab370970efba2fce86ee0c5f794e28d8bca8
zejzhang <zejzhang@iu.edu>
2020-10-20 14:20:02 +0800

    numbers

---
+++ BeginnerTutorial/Numbers.py
@@ -0,0 +1,18 @@
+import math as m
+#print(3)
+#print(3 + 4)
+#print(3 * 4)
+#print(3 * (4+5))
+#print(10 % 3)  #remainder
+
+#my_num = 7
+#print(my_num)
+#print(str(my_num)+" is my favorite number")
+
+print(pow(3,2)) #3^2
+print(max(1,10)) #max one
+print(min(1,10)) #min one
+print(round(7.7)) #四舍五入
+print(m.ceil(7.3)) #进一位
+print(m.floor(7.7)) #退一位
+print(m.sqrt(100)) #squareroot

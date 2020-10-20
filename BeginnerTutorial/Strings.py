f9f8bcd588e97ea5b069d914821fc53cccbae2e7
zejzhang <zejzhang@iu.edu>
2020-10-20 14:03:48 +0800

    strings

---
+++ BeginnerTutorial/Strings.py
@@ -0,0 +1,17 @@
+
+#print("Jason\nZhang")
+#print("Jason\"Zhang")
+
+phrase = "Jason Zhang"
+#         01234
+#print(phrase + " is cool")
+#print(phrase.lower())
+#print(phrase.upper())
+#print(phrase.islower())
+#print(phrase.upper().isupper())
+#print(len(phrase))
+#print(phrase[0])
+#print(phrase[1])
+#print(phrase.index("s"))
+#print(phrase.index("Zh"))
+#print(phrase.replace("Jason", "Zejun"))
